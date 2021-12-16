4e213d86f343b7f8b78e97f08bfc2655751b6b44
efras <renegadexwars@yahoo.com>
2021-12-15 23:36:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-   return "Flask dockerized!!"
+def home():
+   return render_template('index.html')
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+   app.run(debug=True,host='0.0.0.0')
